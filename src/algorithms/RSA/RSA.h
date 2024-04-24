bee8de57c072943ef2fc0f6a7c5994e3fd815770
Bqrry <bqrry4@gmail.com>
2024-04-24 14:22:46 +0300

    RSA workin

--- src/algorithms/RSA/RSA.h
+++ src/algorithms/RSA/RSA.h
@@ -11,8 +11,25 @@ typedef struct{
     mpz_t n, e, d;
 } rsa_keys;
 
-// @param out public_key
-// @param out private_key
-void key_generation(rsa_keys *key);
+typedef struct{
+    mpz_ptr n, e;
+} rsa_pub_key;
+
+typedef struct{
+    mpz_ptr n, d;
+} rsa_prv_key;
+
+/// @brief init state
+void RSA_init();
+/// @brief clear state
+void RSA_clear();
+
+/// @brief Generates RSA keys
+/// @param[out] rsa_keys
+/// @param[in] n size of the prime numbers, key-size/2
+void RSA_key_generation(rsa_keys *key, int n);
+
+void RSA_encrypt(byte_array plaintext, rsa_pub_key key, byte_array *ciphertext);
+void RSA_decrypt(byte_array ciphertext, rsa_prv_key key, byte_array *plaintext);
 
 #endif //SI_PROIECT_RSA_H
--- src/main.c
+++ src/main.c
@@ -2,25 +2,75 @@
 // Created by bqrry on 06.04.2024.
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "algorithms/RSA/RSA.h"
 
 int main() {
+
+    RSA_init();
 
     rsa_keys key;
     mpz_init2(key.e, 2048);
     mpz_init2(key.n, 2048);
     mpz_init2(key.d, 2048);
 
-    key_generation(&key);
+    RSA_key_generation(&key, 1024);
 
-    mpz_out_str(stdout, 10, key.e);
+    mpz_out_str(stdout, 16, key.e);
     printf("\n");
-    mpz_out_str(stdout, 10, key.n);
+    mpz_out_str(stdout, 16, key.n);
     printf("\n");
-    mpz_out_str(stdout, 10, key.d);
+    mpz_out_str(stdout, 16, key.d);
     printf("\n");
-    //mpz_out_str(stdout, 10, key.d);
 
-    mpz_clears(key.e, key.n, key.d, NULL);
+    byte_array a;
+    byte_array b, e;
+
+    a.data = malloc( 4096 *sizeof(char));
+    char ch[] = "I understand that it's an antipattern, but it's convenient.";
+
+    memcpy(a.data, ch, sizeof(ch)/sizeof(char));
+    a.length = sizeof(ch)/sizeof(char);
+
+    b.data = malloc(4096 * sizeof(char));
+    b.length = 4096;
+
+    e.data = malloc(4096 * sizeof(char));
+    e.length = 3;
+
+    rsa_pub_key pub_key;
+    pub_key.n = key.n;
+    pub_key.e = key.e;
+
+    rsa_prv_key prv_key;
+    prv_key.d = key.d;
+    prv_key.n = key.n;
+
+
+    RSA_encrypt(a,pub_key, &b);
+    printf("\nEncrypted: ");
+    for(int i = 0; i < b.length; ++i)
+    {
+        printf("%x", b.data[i]);
+    }
+    printf("\n");
+
+    RSA_decrypt(b, prv_key, &e);
+    printf("Decrypted: %s \nHex: ", e.data);
+
+    for(int i = 0; i < e.length; ++i)
+    {
+        printf("%x", e.data[i]);
+    }
+    printf("\n");
+
+    free(a.data);
+    free(b.data);
+    free(e.data);
+
+    mpz_clears(key.e, key.n, key.d,  NULL);
+
+    RSA_clear();
     return 0;
 }
