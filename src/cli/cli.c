23acbda6a4467307940637cf8095618346de63b2
Radu Olaret <raduolaret1@gmail.com>
2024-05-28 20:34:25 +0300

    More CLI

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -3,6 +3,8 @@
 //
 
 #include <stddef.h>
+#include <malloc.h>
+#include <stdlib.h>
 #include "stdio.h"
 
 #include "cli.h"
@@ -30,6 +32,87 @@ const u_char TROLLFACE[] =
         "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿"
         "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣤⣤⣤⣤⣀⣀⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿";
 
+int get_line_count(char *string) {
+    int count = 1;
+
+    while ((string = strchr(string + 1, '\n')) != NULL) {
+        count++;
+    }
+
+    return count;
+}
+
+void split_options(char *string, char **options_out) {
+    char *option = strtok(string, "\n");
+    int index = 0;
+
+    while (option != NULL) {
+        options_out[index++] = option;
+        option = strtok(NULL, "\n");
+    }
+}
+
+int select_option(int number_of_options, char **options) {
+    printf("Select an option: \n");
+
+    for (int i = 0; i < number_of_options; i++) {
+        printf("\t%d. %s\n", i + 1, options[i]);
+    }
+
+    long selected_option;
+    char read_buffer[32];
+    while (true) {
+        fgets(read_buffer, 32, stdin);
+        selected_option = strtol(read_buffer, NULL, 10);
+
+        if (selected_option <= 0 || selected_option >= number_of_options) {
+            puts("you fool. you absolute baffoon. You think you can challenge me in my own realm? you think you can rebel against my authority? you dare come into my house and upturn my dining chairs and spill coffee grounds in my Keurig? you thought you were safe in your chain mail armor behind that screen of yours. I will take these laminate wood floor boards and destroy you. i didnt want war. but i didnt start it ");
+        } else break;
+    }
+
+    return (int) selected_option - 1;
+}
+
+void scan() {
+    char nmap_temp_buffer[4096], final_buffer[4096];
+
+    while (true) {
+        memset(final_buffer, 0, 4096);
+        FILE *nmap_pipe = popen("nmap -n 192.168.0.0/24 -p 35544 -oG - | awk '/Up$/{print $2}'", "r");
+        printf("Scan started...\n");
+
+        while (fgets(nmap_temp_buffer, 4096, nmap_pipe) != NULL) {
+            strcat(final_buffer, nmap_temp_buffer);
+        }
+
+        printf("Scan finished.\n");
+        if (strlen(final_buffer) < 7) {
+            printf("Scan found no other schizo prophets on this network. Press ENTER to try again.");
+            getchar();
+        } else break;
+    }
+    int line_count = get_line_count(final_buffer);
+
+    char **options = malloc(sizeof(char *) * line_count);
+
+    split_options(final_buffer, options);
+
+    select_option(line_count, options);
+
+    free(options);
+}
+
+void print_progress(int blocks_received, int blocks_total) {
+    char progress_bar[51];
+    memset(progress_bar, '-', 50);
+    progress_bar[50] = '\0';
+
+    int progress = blocks_received * 50 / blocks_total;
+    memset(progress_bar, '#', progress);
+
+    printf("\rProgress: [%s]", progress_bar);
+    fflush(stdout);
+}
 
 void init_cli() {
     struct timespec _100ms_interval;
@@ -39,8 +122,8 @@ void init_cli() {
     puts("\033[1;1H\033[2J");
     printf("Welcome to the Schizo Network©! \n");
 
-    for(int i = 0; i < 21; i++){
-        for (int j = 0; j < 150; j++){
+    for (int i = 0; i < 21; i++) {
+        for (int j = 0; j < 150; j++) {
             u_char char_to_be_written = *(TROLLFACE + ((i * 150 + j) * sizeof(u_char)));
             fputc(char_to_be_written, stdout);
             fflush(stdout);
@@ -49,5 +132,17 @@ void init_cli() {
         printf("\n");
         nanosleep(&_100ms_interval, NULL);
     }
-    printf("Press any key to scan local network for other schizo prophets...\n");
+    printf("Press ENTER to scan local network for other schizo prophets...\n");
+    getchar();
+
+    //scan();
+    for (int i = 0; i <= 100; i++) {
+        print_progress(i,100);
+        for (int j = 0; j < 150; j++) {
+            nanosleep(&_100ms_interval, NULL);
+        }
+    }
 }
+
+
+
